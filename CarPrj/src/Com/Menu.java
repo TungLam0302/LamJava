b4fd36110a3df771f7b3a58e27faa36d6c2c1b02
TungLam0302 <tunglam0982243286@gmail.com>
2024-10-15 14:46:21 +0700

    Menu

--- CarPrj/src/Com/Menu.java
+++ CarPrj/src/Com/Menu.java
@@ -2,4 +2,18 @@ package Com;
 
 public class Menu {
     
+    public void displayMenu(){
+        System.out.println("1- List all brands");
+        System.out.println("2- Add a new brand");
+        System.out.println("3- Search a brand based on its ID");
+        System.out.println("4- Update a brand");
+        System.out.println("5- Save brands to the file, named brands.txt");
+        System.out.println("6- List all cars in ascending order of brand names");
+        System.out.println("7- List cars based on a part of an input brand name");
+        System.out.println("8- Add a car");
+        System.out.println("9- Remove a car based on its ID");
+        System.out.println("10- Update a car based on its ID");
+        System.out.println("11- Save cars to file, named cars.txt");
+    }
+
 }
--- CarPrj/src/main.java
+++ CarPrj/src/main.java
@@ -1,4 +1,12 @@
 
+import Com.Menu;
+
+
 public class main {
-    
+    public static void main(String[] args) {
+        Menu mn = new Menu();
+        mn.displayMenu();
+        
+        
+    }
 }
