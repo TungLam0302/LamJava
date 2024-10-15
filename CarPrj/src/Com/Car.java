2e5563f6cbc5cb2e37577638b7360920124ad961
TungLam0302 <tunglam0982243286@gmail.com>
2024-10-15 13:52:50 +0700

    15/10/2024 Lam: add project Car Manager(assignment)

---
+++ CarPrj/src/Com/Car.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Com;
+/**
+ *
+ * @author ADMIN
+ */
+public class Car {
+    
+}
