2b63f0fcd65fc806d3de474ac7eacb8636bc5186
TungLam0302 <tunglam0982243286@gmail.com>
2024-10-15 14:03:38 +0700

    lamdeptrai

--- CarPrj/src/Com/Brand.java
+++ CarPrj/src/Com/Brand.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Com;
 
-/**
- *
- * @author ADMIN
- */
 public class Brand {
     
 }
